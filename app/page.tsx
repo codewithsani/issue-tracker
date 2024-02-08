fad31eb1ac2e8731ef29ee849d0045fa3f6ac687
Saied Sani <saied.sani.biderooni@gmail.com>
2024-02-08 18:31:40 +0100

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  console.log(parseInt(searchParams.page));
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
