e0b954c5c2f135f6af533dc37e5d4f1ec73009dc
Saied Sani <saied.sani.biderooni@gmail.com>
2024-02-08 12:06:18 +0100

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  console.log(parseInt(searchParams.page));
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
